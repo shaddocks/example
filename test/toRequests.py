8ee1db99c4d5a2cef4f265ebd2fdd7f03449c4c6
shaddocks <m15926539316@163.com>
2020-09-20 10:17:46 +0800

    Initial commit

---
+++ test/toRequests.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# __time__: 2020/9/19 下午9:36
+# __author__: xu
+# __file__: toRequests.py
+
+import requests
+import random
+
+list = []
+random.shuffle(list)
+
+print(list)
